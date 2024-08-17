454a31f491df40c837c927e8262136397f092518
AmalpisF <amalpisfuenmayor@gmail.com>
2024-08-17 10:54:04 -0400

    clase9

--- post/views.py
+++ post/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from  .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
 
 class HomePageView(ListView):
     model = Post
     template_name = "home.html"
+
+class DetailPageView(DetailView):
+    model = Post
+    template_name = "detail.html"
